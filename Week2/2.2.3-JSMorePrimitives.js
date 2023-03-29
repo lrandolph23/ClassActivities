6026747c4a128700129ee2bb566e62210a7b3845
lrandolph23 <lacy.randolph23@gmail.com>
2023-03-29 09:25:08 -0500

    Homework

---
+++ Week2/2.2.3-JSMorePrimitives.js
@@ -0,0 +1,27 @@
+// Create a variable "timeOfDay" and assign it a string
+let timeOfDay = "noon";
+// Create a variable "greeting" that references a template literal
+let greeting = `Hello, it is currently ${timeOfDay}`;
+// Use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+
+// Print "greeting"
+
+// Create a new variable, but do not assign it a value
+let newVar
+// Print the new variable and its type
+//console.log(typeof newVar);
+// What type should we expect?
+
+// Assign the variable a value that indicates the variable is purposely blank
+newVar = "";
+// What value should we assign?
+console.log(typeof newVar);
+// Print the variable and its type again
+// What type should we expect?
+
+// Try to print a variable that does not exist
+console.log(brain);
+// What should we expect to print in the CLI?
+
+// Print "greeting" again
+// Will this line run?
